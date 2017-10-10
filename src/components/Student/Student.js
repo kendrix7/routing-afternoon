cf1637c71f94b75a69eb52badc543f926c4c2d85
Mark Nielsen <marknielsen7@outlook.com>
2017-10-10 16:13:52 -0600

    finished

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default class Student extends Component {
-    constructor() {
-      super()
+  constructor() {
+    super()
 
+    this.state = {
+      studentInfo: {}
     }
 
-    render() {
-      return (
-        <div className="box">
-          <h1>Student</h1>
-        </div>
-      )
-    }
+  }
+
+  componentDidMount() {
+    return axios.get(`http://localhost:3005/students/${this.props.match.params.id}`).then(res => {
+      this.setState({
+        studentInfo: res.data
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="box">
+        <h1>Student</h1>
+        <h1>
+          {this.state.studentInfo.first_name} {this.state.studentInfo.last_name}
+        </h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
+        <Link to={`/classlist/${this.state.studentInfo.class}`}><button>Back</button></Link>
+      </div>
+    )
+  }
 }
